d11c5406d4f670f64096712b055ddf6289f24e1d
Shreet1 <68271749+Shreet1@users.noreply.github.com>
2025-08-06 19:51:35 +0530

    Delete add_vendor.php

--- add_vendor.php
+++
@@ -1,9 +0,0 @@
-<form method="post" action="save_vendor.php">
-  <label>Name:</label><input name="name"><br>
-  <label>Location:</label><input name="location"><br>
-  <label>Whatsapp:</label><input name="whatsapp"><br>
-  <label>Route Served:</label><input name="route"><br>
-  <label>Vehicle Type:</label><input name="vehicle_type"><br>
-  <button type="submit">Add Vendor</button>
-</form>
-
